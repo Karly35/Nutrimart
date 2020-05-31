49cdb04333451b7cc6d3b40fb0dd4f64a58ab4d6
Karly35 <ckarlyn96@yahoo.fr>
2020-05-31 11:55:56 -0400

    Add files via upload

---
+++ scripts/groceries.js
@@ -0,0 +1,122 @@
+// Array of products, each product is an object with different fieldset
+// A set of ingredients should be added to products		 
+
+var products = [
+	{
+		name: "brocoli",
+		vegetarian: true,
+		glutenFree: true,
+		price: 1.99,
+		organic: true,
+		vegetarianGlutenFree: true
+
+	},
+	{
+		name: "bread",
+		vegetarian: true,
+		glutenFree: false,
+		price: 2.35,
+		organic: false,
+		vegetarianGlutenFree: false
+	},
+	{
+		name: "salmon",
+		vegetarian: false,
+		glutenFree: true,
+		price: 10.00,
+		organic: true,
+		vegetarianGlutenFree: false
+	},
+	{
+		name: "banana",
+		vegetarian: true,
+		glutenFree: true,
+		price: 2.19,
+		organic: true,
+		vegetarianGlutenFree: true
+	},
+	{
+		name: "cereals",
+		vegetarian: false,
+		glutenFree: false,
+		price: 2.99,
+		organic: false,
+		vegetarianGlutenFree: false
+	},
+	{
+		name: "pasta",
+		vegetarian: true,
+		glutenFree: false,
+		price: 1.99,
+		organic: false,
+		vegetarianGlutenFree: false
+	},
+	{
+		name: "eggs",
+		vegetarian: false,
+		glutenFree: true,
+		price: 5.99,
+		organic: true,
+		vegetarianGlutenFree: false
+	},
+	{
+		name: "rice",
+		vegetarian: true,
+		glutenFree: true,
+		price: 12.99,
+		organic: false,
+		vegetarianGlutenFree: true
+	},
+	{
+		name: "milk",
+		vegetarian: false,
+		glutenFree: true,
+		price: 4.99,
+		organic: false,
+		vegetarianGlutenFree: false
+	},
+	{
+		name: "greek yogurt",
+		vegetarian: true,
+		glutenFree: true,
+		price: 6.19,
+		organic: true,
+		vegetarianGlutenFree: true
+	}
+];
+	
+
+
+// given restrictions provided, make a reduced list of products
+// prices should be included in this list, as well as a sort based on price
+
+function restrictListProducts(prods, restriction) {
+	let product_names = [];
+	for (let i=0; i<prods.length; i+=1) {
+		
+		if ((restriction == "VegetarianGlutenFree") && (prods[i].vegetarianGlutenFree == true)){
+			product_names.push([prods[i].name,prods[i].price]); 
+		}
+		else if ((restriction == "Vegetarian") && (prods[i].vegetarian == true)){
+			product_names.push([prods[i].name,prods[i].price]); 
+		}
+		else if ((restriction == "GlutenFree") && (prods[i].glutenFree == true)){
+			product_names.push([prods[i].name ,prods[i].price]);
+		}
+		else if (restriction == "None"){
+			product_names.push([prods[i].name, prods[i].price]);
+		}
+	}
+	return product_names;
+}
+
+// Calculate the total price of items, with received parameter being a list of products
+function getTotalPrice(chosenProducts) {
+	totalPrice = 0;
+	for (let i=0; i<products.length; i+=1) {
+		if (chosenProducts.indexOf(products[i].name) > -1){
+			totalPrice += products[i].price;
+		}
+	}
+	return totalPrice;
+}
